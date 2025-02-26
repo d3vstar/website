7930873593a4be31f4670f0ed6e8b88cbbd32c74
Augusto Gerardo Sotelo Labarca <agsotelo@gmail.com>
2025-02-26 01:07:47 -0300

    feat: added dynamic validation support depending on is_parent check

--- components/Home/actions.ts
+++ components/Home/actions.ts
@@ -4,19 +4,67 @@ import { z } from "zod";
 
 const subscriptionRequestSchema = z.object({
       student_motivation: z.string().min(1, { message: "Requerido: Indicar sus expectativas de la academia" }),
-      student_fullname: z.string().min(1, { message: "Requerido: Nombre completo del estudiante" }),
-      student_birthday: z.string().min(1, { message: "Requerido: Fecha de nacimiento del estudiante" }),
-      student_id: z.string().min(1, { message: "Requerido: Cédula de Identidad del estudiante" }),
-      student_email: z.string().min(1, { message: "Requerido: Correo del estudiante" }),
-      student_phone: z.string().min(1, { message: "Requerido: Teléfono de contacto del estudiante" }),
+      student_fullname: z.string().min(1, { message: "Requerido: Nombre completo del postulante" }),
+      student_birthday: z.string().min(1, { message: "Requerido: Fecha de nacimiento del postulante" }),
+      student_id: z.string().min(1, { message: "Requerido: Cédula de Identidad del postulante" }),
+      student_email: z.string().optional(),
+      student_phone: z.string().optional(),
       is_parent: z.boolean(),
-      parent_fullname: z.string().min(1, { message: "Requerido: Nombre completo del apoderado" }),
-      parent_id: z.string().min(1, { message: "Requerido: Cédula de Identidad del apoderado" }),
-      parent_email: z.string().min(1, { message: "Requerido: Correo del apoderado" }),
-      parent_phone: z.string().min(1, { message: "Requerido: Teléfono del apoderado" }),
+      parent_fullname: z.string().optional(),
+      parent_id: z.string().optional(),
+      parent_email: z.string().optional(),
+      parent_phone: z.string().optional(),
       emergency_fullname: z.string().min(1, { message: "Requerido: Nombre completo del contacto de emergencia" }),
       emergency_relationship_type: z.string().min(1, { message: "Requerido: Tipo de parentesco del contacto de emergencia" }),
       emergency_phone: z.string().min(1, { message: "Requerido: Teléfono del contacto emergencia" }),
+  }).refine( (data) => {
+    const { is_parent } = data;
+    const student_email = data.student_email || "";
+
+    return is_parent ? student_email.length > 0 : student_email.length >= 0;
+  }, {
+    message: "Requerido: correo de postulante mayor de edad",
+    path: ['student_email']
+  }).refine( (data) => {
+    const { is_parent } = data;
+    const student_phone = data.student_phone || "";
+
+    return is_parent ? student_phone.length > 0 : student_phone.length >= 0;
+  }, {
+    message: "Requerido: Teléfono de contacto del postulante mayor de edad",
+    path: ['student_phone']
+  }).refine( (data) => {
+    const { is_parent } = data;
+    const parent_fullname = data.parent_fullname || "";
+
+    return !is_parent ? parent_fullname.length > 0 : parent_fullname.length >= 0;
+  }, {
+    message: "Requerido: Nombre completo del apoderado",
+    path: ['parent_fullname']
+  }).refine( (data) => {
+    const { is_parent } = data;
+    const parent_id = data.parent_id || "";
+
+    return !is_parent ? parent_id.length > 0 : parent_id.length >= 0;
+  }, {
+    message: "Requerido: Cédula de Identidad del apoderado",
+    path: ['parent_id']
+  }).refine( (data) => {
+    const { is_parent } = data;
+    const parent_email = data.parent_email || "";
+
+    return !is_parent ? parent_email.length > 0 : parent_email.length >= 0;
+  }, {
+    message: "Requerido: Correo del apoderado",
+    path: ['parent_email']
+  }).refine( (data) => {
+    const { is_parent } = data;
+    const parent_phone = data.parent_phone || "";
+
+    return !is_parent ? parent_phone.length > 0 : parent_phone.length >= 0;
+  }, {
+    message: "Requerido: Teléfono del apoderado",
+    path: ['parent_phone']
   });
   
   export type subscriptionRequestActionState = {
@@ -68,11 +116,11 @@ const subscriptionRequestSchema = z.object({
         const emergency_relationship_type = form.get("emergency_relationship_type") as string;
         const emergency_phone = form.get("emergency_phone") as string;
 
-        console.log(`Value of is_parent:${is_parent}`);
-        /**
-         * TODO: Logic implementation if is_parent=true/false
-         */
-        const validatedFields = subscriptionRequestSchema.safeParse({ 
+
+        console.log(`IsParent: ${is_parent}`);
+
+        let validatedFields = subscriptionRequestSchema.safeParse(
+          { 
             student_motivation,
             student_fullname,
             student_birthday,
@@ -86,9 +134,14 @@ const subscriptionRequestSchema = z.object({
             parent_phone,
             emergency_fullname,
             emergency_relationship_type,
-            emergency_phone,
-        });
+            emergency_phone
+          });
 
+          console.log(`Validation Fields result: ${validatedFields.success}`);
+          
+          if (!validatedFields.success) {
+            console.log(`Errors: ${JSON.stringify(validatedFields.error.flatten().fieldErrors)}`)
+          }
         if (!validatedFields.success) {
             return {
                 student_motivation,
@@ -134,6 +187,7 @@ const subscriptionRequestSchema = z.object({
     }
 
     // If validation passes, you can send the data to your API or process it
+    console.log("Call to Subscription Request Service in progress...");
     const response = await fetch(serviceSubscriptionRequestUrl, {
       method: 'POST',
       headers: {
