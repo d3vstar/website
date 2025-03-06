a1a7ab6640edd5ff929fdaf964f30aff95ba7716
Augusto Gerardo Sotelo Labarca <agsotelo@gmail.com>
2025-03-06 17:28:10 -0300

    feature: enable Application Default Credentials

--- README.md
+++ README.md
@@ -31,35 +31,54 @@ To learn more about Next.js, take a look at the following resources:
 
 ### Secrets:
 
-```bash
-$gcloud secrets create GOOGLE_CLOUD_FUNCTION_CREDENTIALS \
---replication-policy="user-managed" \
---locations=us-central1 \
---data-file="./credentials.json" \
---project="$(gcloud config get-value project)" 
+- First create a service account with less privilege (roles: Google clud functions invokers and Cloud Run invoker)
+- Use Application Default Credentials as Authentication strategy (use case: for smae authority on every call)
+    - Impersonate the service account for local development in order to use ADC (Application Default Credentials strategy)
 
-Created version [1] of the secret [GOOGLE_CLOUD_FUNCTION_CREDENTIALS].
+    ```bash
+    $ gcloud auth application-default login --impersonate-service-account=<SERVICE_ACCOUNT_EMAIL>
+    Your browser has been opened to visit:
 
-**NOTE:** Make sure to be in the right project since the above command will get the value from the current one.
+        https://accounts.google.com/o/oauth2/auth?response_type=code&client_id=764086051850-6qr4p6gpi6hn506pt8ejuq83di341hur.apps.googleusercontent.com&redirect_uri=http%3A%2F%2Flocalhost%3A8085%2F&scope=openid+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcloud-platform+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fsqlservice.login&state=1YtfpcuA3mdXXIKzkU3PCa0Mh2AI0R&access_type=offline&code_challenge=-RqRzvD3fSuFl9goS-IzSVxAIsB7Bu19FytK3h_HfJg&code_challenge_method=S256
 
-**NOTE:** We use user-managed and 1 location replication policy for reducing cost (different use case will require change this behavior)
-```
 
-Grant access to app hosting:
-```bash
-$ firebase apphosting:secrets:grantaccess GOOGLE_CLOUD_FUNCTION_CREDENTIALS --backend <BACKEND_NAME>
-✔  Successfully set IAM bindings on secret GOOGLE_CLOUD_FUNCTION_CREDENTIALS.
-```
+    Credentials saved to file: [/Users/d3vrockz/.config/gcloud/application_default_credentials.json]
+
+    These credentials will be used by any library that requests Application Default Credentials (ADC).
+    ```
+    - Create variable in secret manager and specify the reference in apphosting.yaml
+
+    ```bash
+    $ gcloud secrets create GOOGLE_APPLICATION_CREDENTIALS \  
+    --replication-policy="user-managed" \
+    --locations=us-central1 \
+    --data-file="./credentials.json" \
+    --project="$(gcloud config get-value project)"
+    Created version [1] of the secret [GOOGLE_APPLICATION_CREDENTIALS].
+    ```
+    **NOTE:** Make sure to be in the right project since the above command will get the value from the current one.
+
+    **NOTE:** We use user-managed and 1 location replication policy for reducing cost (different use case will require change this behavior)
+
+    - Grant access to app hosting using firebase CLI:
+
+    ```bash
+    $ firebase apphosting:secrets:grantaccess GOOGLE_APPLICATION_CREDENTIALS --backend <BACKEND_NAME>
+    ✔  Successfully set IAM bindings on secret GOOGLE_APPLICATION_CREDENTIALS.
+    ```
+
 
 Verify the secret and its contents
 
 ```bash
-$ gcloud secrets versions list GOOGLE_CLOUD_FUNCTION_CREDENTIALS --project="$(gcloud config get-value project)"
+$ gcloud secrets versions list GOOGLE_APPLICATION_CREDENTIALS --project="$(gcloud config get-value project)"
+NAME  STATE    CREATED              DESTROYED
+1     enabled  2025-03-06T20:15:24  -
 ```
 
 Validate access to the secret:
 ```bash
-$ gcloud secrets versions access 1 --secret=GOOGLE_CLOUD_FUNCTION_CREDENTIALS --project="$(gcloud config get-value project)"
+$ gcloud secrets versions access 1 --secret=GOOGLE_APPLICATION_CREDENTIALS --project="$(gcloud config get-value project)"
 ```
 
 Before run set the environment variable in your terminal as follow:
--- components/Home/actions.ts
+++ components/Home/actions.ts
@@ -1,123 +1,125 @@
 "use server"
+
 import { GoogleAuth } from 'google-auth-library';
 import { z } from "zod";
 
 const subscriptionRequestSchema = z.object({
-      student_motivation: z.string().min(1, { message: "Requerido: Indicar sus expectativas de la academia" }),
-      student_fullname: z.string().min(1, { message: "Requerido: Nombre completo del postulante" }),
-      student_birthday: z.string().min(1, { message: "Requerido: Fecha de nacimiento del postulante" }),
-      student_id: z.string().min(1, { message: "Requerido: Cédula de Identidad del postulante" }),
-      student_email: z.string().optional(),
-      student_phone: z.string().optional(),
-      is_parent: z.boolean(),
-      parent_fullname: z.string().optional(),
-      parent_id: z.string().optional(),
-      parent_email: z.string().optional(),
-      parent_phone: z.string().optional(),
-      emergency_fullname: z.string().min(1, { message: "Requerido: Nombre completo del contacto de emergencia" }),
-      emergency_relationship_type: z.string().min(1, { message: "Requerido: Tipo de parentesco del contacto de emergencia" }),
-      emergency_phone: z.string().min(1, { message: "Requerido: Teléfono del contacto emergencia" }),
-  }).refine( (data) => {
+    student_motivation: z.string().min(1, { message: "Requerido: Indicar sus expectativas de la academia" }),
+    student_fullname: z.string().min(1, { message: "Requerido: Nombre completo del postulante" }),
+    student_birthday: z.string().min(1, { message: "Requerido: Fecha de nacimiento del postulante" }),
+    student_id: z.string().min(1, { message: "Requerido: Cédula de Identidad del postulante" }),
+    student_email: z.string().optional(),
+    student_phone: z.string().optional(),
+    is_parent: z.boolean(),
+    parent_fullname: z.string().optional(),
+    parent_id: z.string().optional(),
+    parent_email: z.string().optional(),
+    parent_phone: z.string().optional(),
+    emergency_fullname: z.string().min(1, { message: "Requerido: Nombre completo del contacto de emergencia" }),
+    emergency_relationship_type: z.string().min(1, { message: "Requerido: Tipo de parentesco del contacto de emergencia" }),
+    emergency_phone: z.string().min(1, { message: "Requerido: Teléfono del contacto emergencia" }),
+}).refine((data) => {
     const { is_parent } = data;
     const student_email = data.student_email || "";
 
     return is_parent ? student_email.length > 0 : student_email.length >= 0;
-  }, {
+}, {
     message: "Requerido: correo de postulante mayor de edad",
     path: ['student_email']
-  }).refine( (data) => {
+}).refine((data) => {
     const { is_parent } = data;
     const student_phone = data.student_phone || "";
 
     return is_parent ? student_phone.length > 0 : student_phone.length >= 0;
-  }, {
+}, {
     message: "Requerido: Teléfono de contacto del postulante mayor de edad",
     path: ['student_phone']
-  }).refine( (data) => {
+}).refine((data) => {
     const { is_parent } = data;
     const parent_fullname = data.parent_fullname || "";
 
     return !is_parent ? parent_fullname.length > 0 : parent_fullname.length >= 0;
-  }, {
+}, {
     message: "Requerido: Nombre completo del apoderado",
     path: ['parent_fullname']
-  }).refine( (data) => {
+}).refine((data) => {
     const { is_parent } = data;
     const parent_id = data.parent_id || "";
 
     return !is_parent ? parent_id.length > 0 : parent_id.length >= 0;
-  }, {
+}, {
     message: "Requerido: Cédula de Identidad del apoderado",
     path: ['parent_id']
-  }).refine( (data) => {
+}).refine((data) => {
     const { is_parent } = data;
     const parent_email = data.parent_email || "";
 
     return !is_parent ? parent_email.length > 0 : parent_email.length >= 0;
-  }, {
+}, {
     message: "Requerido: Correo del apoderado",
     path: ['parent_email']
-  }).refine( (data) => {
+}).refine((data) => {
     const { is_parent } = data;
     const parent_phone = data.parent_phone || "";
 
     return !is_parent ? parent_phone.length > 0 : parent_phone.length >= 0;
-  }, {
+}, {
     message: "Requerido: Teléfono del apoderado",
     path: ['parent_phone']
-  });
-  
-  export type subscriptionRequestActionState = {
-      student_motivation?: string;
-      student_fullname?: string;
-      student_birthday?: string;
-      student_id?: string;
-      student_email?: string;
-      student_phone?: string;
-      is_parent?: boolean;
-      parent_fullname?: string;
-      parent_id?: string;
-      parent_email?: string;
-      parent_phone?: string;
-      emergency_fullname?: string;
-      emergency_relationship_type?: string;
-      emergency_phone?: string;
-      success?: string;
+});
+
+export type subscriptionRequestActionState = {
+    student_motivation?: string;
+    student_fullname?: string;
+    student_birthday?: string;
+    student_id?: string;
+    student_email?: string;
+    student_phone?: string;
+    is_parent?: boolean;
+    parent_fullname?: string;
+    parent_id?: string;
+    parent_email?: string;
+    parent_phone?: string;
+    emergency_fullname?: string;
+    emergency_relationship_type?: string;
+    emergency_phone?: string;
+    success?: string;
     errors?: {
-      student_motivation?: string[];
-      student_fullname?: string[];
-      student_birthday?: string[];
-      student_id?: string[];
-      student_email?: string[];
-      student_phone?: string[];
-      parent_fullname?: string[];
-      parent_id?: string[];
-      parent_email?: string[];
-      parent_phone?: string[];
-      emergency_fullname?: string[];
-      emergency_relationship_type?: string[];
-      emergency_phone?: string[];
+        student_motivation?: string[];
+        student_fullname?: string[];
+        student_birthday?: string[];
+        student_id?: string[];
+        student_email?: string[];
+        student_phone?: string[];
+        parent_fullname?: string[];
+        parent_id?: string[];
+        parent_email?: string[];
+        parent_phone?: string[];
+        emergency_fullname?: string[];
+        emergency_relationship_type?: string[];
+        emergency_phone?: string[];
     };
-  };
+};
 
-  export async function subscriptionRequestAction(_prevState: subscriptionRequestActionState, form: FormData): Promise<subscriptionRequestActionState> {
-        const student_motivation = form.get("student_motivation") as string;
-        const student_fullname = form.get("student_fullname") as string;
-        const student_birthday = form.get("student_birthday") as string;
-        const student_id = form.get("student_id") as string;
-        const student_email = form.get("student_email") as string;
-        const student_phone = form.get("student_phone") as string;
-        const is_parent = (form.get("is_parent") == "on") as boolean;
-        const parent_fullname = form.get("parent_fullname") as string;
-        const parent_id = form.get("parent_id") as string;
-        const parent_email = form.get("parent_email") as string;
-        const parent_phone = form.get("parent_phone") as string;
-        const emergency_fullname = form.get("emergency_fullname") as string;
-        const emergency_relationship_type = form.get("emergency_relationship_type") as string;
-        const emergency_phone = form.get("emergency_phone") as string;
 
-        const validatedFields = subscriptionRequestSchema.safeParse(
-          { 
+export async function subscriptionRequestAction(_prevState: subscriptionRequestActionState, form: FormData): Promise<subscriptionRequestActionState> {
+    const student_motivation = form.get("student_motivation") as string;
+    const student_fullname = form.get("student_fullname") as string;
+    const student_birthday = form.get("student_birthday") as string;
+    const student_id = form.get("student_id") as string;
+    const student_email = form.get("student_email") as string;
+    const student_phone = form.get("student_phone") as string;
+    const is_parent = (form.get("is_parent") == "on") as boolean;
+    const parent_fullname = form.get("parent_fullname") as string;
+    const parent_id = form.get("parent_id") as string;
+    const parent_email = form.get("parent_email") as string;
+    const parent_phone = form.get("parent_phone") as string;
+    const emergency_fullname = form.get("emergency_fullname") as string;
+    const emergency_relationship_type = form.get("emergency_relationship_type") as string;
+    const emergency_phone = form.get("emergency_phone") as string;
+
+    const validatedFields = subscriptionRequestSchema.safeParse(
+        {
             student_motivation,
             student_fullname,
             student_birthday,
@@ -132,65 +134,74 @@ const subscriptionRequestSchema = z.object({
             emergency_fullname,
             emergency_relationship_type,
             emergency_phone
-          });
+        });
 
-          console.log(`Validation Fields result success? : ${validatedFields.success}`);
-          
-          if (!validatedFields.success) {
-            console.log(`Errors: ${JSON.stringify(validatedFields.error.flatten().fieldErrors)}`)
-          }
-        if (!validatedFields.success) {
+    console.log(`Validation Fields result success? : ${validatedFields.success}`);
 
-          return {...validatedFields, success: 'validation', errors: validatedFields.error.flatten().fieldErrors}
-        }
+    if (!validatedFields.success) {
+        console.log(`Errors: ${JSON.stringify(validatedFields.error.flatten().fieldErrors)}`)
+    }
+    if (!validatedFields.success) {
+
+        return {
+            ...validatedFields,
+            errors: validatedFields.error.flatten().fieldErrors,
+            success: 'validation'
+        };
+    }
+
+    /**
+     * Check environment variables
+     */
+    const GOOGLE_CLOUD_FUNCTION_TARGET_AUDIENCE = process.env.GOOGLE_CLOUD_FUNCTION_TARGET_AUDIENCE || "";
+
+    if (!GOOGLE_CLOUD_FUNCTION_TARGET_AUDIENCE) {
+        console.error("Error accessing variable GOOGLE_CLOUD_FUNCTION_TARGET_AUDIENCE");
+        return {
+            ...validatedFields,
+            success: 'fail'
+        };
+    }
+
+    try {
+        // Log the raw string:
+        console.log('GOOGLE_APPLICATION_CREDENTIALS):', process.env.GOOGLE_APPLICATION_CREDENTIALS);
+        console.log(`Audience: ${GOOGLE_CLOUD_FUNCTION_TARGET_AUDIENCE}`);
 
         /**
-         * Check environment variables
+         * Since we are running in a managed environment (App Hosting). Google Cloud automatically provides 
+         * application default credentials (ADC) or is injecting the secret to your process.env variable. 
+         * Your application does not need to handle the credentials, but only the target audience.
          */
-        const GOOGLE_CLOUD_FUNCTION_TARGET_AUDIENCE = process.env.GOOGLE_CLOUD_FUNCTION_TARGET_AUDIENCE || "";
-        
-        if (!GOOGLE_CLOUD_FUNCTION_TARGET_AUDIENCE) {
-          console.error("Error accessing variable GOOGLE_CLOUD_FUNCTION_TARGET_AUDIENCE");
-          return {...validatedFields, success: 'fail'};
-        }
+        const auth = new GoogleAuth();
 
-        try {
-          // Log the raw string:
-          console.log('GOOGLE_CLOUD_FUNCTION_CREDENTIALS):', process.env.GOOGLE_CLOUD_FUNCTION_CREDENTIALS);
-          console.log(`Audience: ${GOOGLE_CLOUD_FUNCTION_TARGET_AUDIENCE}`);
+        const idTokenClient = await auth.getIdTokenClient(GOOGLE_CLOUD_FUNCTION_TARGET_AUDIENCE);
+        const idToken = await idTokenClient.idTokenProvider.fetchIdToken(GOOGLE_CLOUD_FUNCTION_TARGET_AUDIENCE);
 
-          // const auth = new GoogleAuth({
-          //     credentials, // path.join(process.cwd(), GOOGLE_CLOUD_FUNCTION_CREDENTIALS_FILE),
-          //   });
-      
-          /**
-           * Since we are running in a managed environment (App Hosting). Google Cloud automatically provides 
-           * application default credentials (ADC) or is injecting the secret to your process.env variable. 
-           * Your application does not need to handle the credentials, but only the target audience.
-           */
-          const auth = new GoogleAuth();
-
-          const idTokenClient = await auth.getIdTokenClient(GOOGLE_CLOUD_FUNCTION_TARGET_AUDIENCE);
-          const idToken = await idTokenClient.idTokenProvider.fetchIdToken(GOOGLE_CLOUD_FUNCTION_TARGET_AUDIENCE);
-          
-          // Make a request to the target audience URL
-          const res = await idTokenClient.request({ 
-            url: GOOGLE_CLOUD_FUNCTION_TARGET_AUDIENCE, 
+        // Make a request to the target audience URL
+        const res = await idTokenClient.request({
+            url: GOOGLE_CLOUD_FUNCTION_TARGET_AUDIENCE,
             method: 'POST',
             headers: {
-              'Content-Type': 'application/json',
-              'Authorization': `Bearer ${idToken}`
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${idToken}`
             },
             data: validatedFields.data,
-          });
-      
-        console.log('Service response:', res.data);
-      
-        } catch(error) {
-          console.error(error);
-      
-          return {...validatedFields, success: 'fail'};
-        }
+        });
 
-    return { ...validatedFields, success: 'true'};
-  }
+        console.log('Service response:', res.data);
+
+    } catch (error) {
+        console.error(error);
+
+        return {
+            ...validatedFields,
+            success: 'fail'
+        };
+    }
+
+    return {
+        ...validatedFields,
+        success: 'true'
+    };
+}
